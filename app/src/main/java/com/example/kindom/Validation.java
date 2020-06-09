acf98a402cb568ff94e929677e42d00e186bd0f1
SONGYI <e0392524@u.nus.edu>
2020-06-10 00:02:38 +0800

    Add email and password validation

---
+++ app/src/main/java/com/example/kindom/Validation.java
@@ -0,0 +1,29 @@
+package com.example.kindom;
+
+import android.text.TextUtils;
+import android.util.Patterns;
+
+import java.util.regex.Pattern;
+
+public class Validation {
+
+    private static final Pattern PATTERN_PASSWORD = Pattern.compile(
+            "^" +
+            "(?=.*[A-Z])" +    // at least 1 uppercase letter
+            "(?=.*[a-z])" +    // at least 1 lowercase letter
+            "(?=.*[0-9])" +    // at least 1 number
+            ".{8,}");          // at least 8 characters
+
+
+    public static boolean isValidEmail(CharSequence target) {
+        return (!TextUtils.isEmpty(target) && Patterns.EMAIL_ADDRESS.matcher(target).matches());
+    }
+
+    public static boolean isNonEmptyPassword(CharSequence target) {
+        return !TextUtils.isEmpty(target);
+    }
+
+    public static boolean isValidPassword(CharSequence targert) {
+        return isNonEmptyPassword(targert) && PATTERN_PASSWORD.matcher(targert).matches();
+    }
+}
