3e7cc50708359d4cafaf950d179862145e1045c0
SONGYI <e0392524@u.nus.edu>
2020-06-10 15:12:20 +0800

    Add user group (admin / user) input

--- app/src/main/java/com/example/kindom/RegisterProfileActivity.java
+++ app/src/main/java/com/example/kindom/RegisterProfileActivity.java
@@ -1,29 +1,107 @@
 package com.example.kindom;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.material.textfield.TextInputLayout;
+
 public class RegisterProfileActivity extends AppCompatActivity {
+
+    private TextInputLayout mNameField;
+    private TextInputLayout mPostalCodeField;
+    private boolean isValidName;
+    private boolean isValidPostalCode;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_profile);
 
+        mNameField = findViewById(R.id.edit_name);
+        mPostalCodeField = findViewById(R.id.edit_postal_code);
+
+        // Check name and postal code validity
+        mNameField.getEditText().addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                // Do nothing
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                // Do nothing
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                if (!Validation.isNonEmpty(s)) {
+                    isValidName = false;
+                    mNameField.setError(getString(R.string.error_name));
+                } else {
+                    isValidName = true;
+                    mNameField.setError(null);
+                }
+            }
+        });
+        mPostalCodeField.getEditText().addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                // Do nothing
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                // Do nothing
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                // TODO: Check postal code validity
+                if (!Validation.isNonEmpty(s)) {
+                    isValidPostalCode = false;
+                    mPostalCodeField.setError(getString(R.string.error_postal_code));
+                } else {
+                    isValidPostalCode = true;
+                    mPostalCodeField.setError(null);
+                }
+            }
+        });
+
         Button nextButton = findViewById(R.id.next_button);
         nextButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(RegisterProfileActivity.this, RegisterAccountActivity.class);
-                startActivity(intent);
+                if (isValidName && isValidPostalCode) {
+                    Intent intent = new Intent(RegisterProfileActivity.this, RegisterAccountActivity.class);
+                    startActivity(intent);
+                } else {
+                    showAlertDialog();
+                }
             }
         });
+    }
 
-        // TODO: Check postal code validity
+    /**
+     * Shows alert dialog
+     */
+    private void showAlertDialog() {
+        new AlertDialog.Builder(this)
+                .setMessage(R.string.error_profile)
+                .setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        // User clicked OK button
+                    }
+                })
+                .show();
     }
 
     @Override
