dd2eee77d3cb8ad881af79eb518f16495e0ad661
Song Yi <song.yi.ang.98@gmail.com>
2020-06-18 03:47:50 +0800

    Validate user inputs in HelpMePostAddActivity

--- app/src/main/java/com/example/kindom/HelpMePostAddActivity.java
+++ app/src/main/java/com/example/kindom/HelpMePostAddActivity.java
@@ -1,13 +1,17 @@
 package com.example.kindom;
 
 import android.app.Activity;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 
 import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.DialogFragment;
@@ -15,6 +19,8 @@ import androidx.fragment.app.DialogFragment;
 import com.google.android.material.button.MaterialButton;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.Calendar;
 
@@ -22,8 +28,14 @@ public class HelpMePostAddActivity extends AppCompatActivity {
 
     private String[] CATEGORIES = new String[]{"Care", "Food", "Groceries", "Others"};
 
+    private DatabaseReference mHelpMePostDatabaseReference;
     private int[] mDate;
     private int[] mTime;
+    private boolean isValidCategory = false;
+    private boolean isValidTitle = false;
+    private boolean isValidDate = false;
+    private boolean isNonEmptyTime = false;
+    private boolean isValidTime = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,8 +55,13 @@ public class HelpMePostAddActivity extends AppCompatActivity {
 
         // Initialize user's inputs
         setCategoryDropdownMenu();
+        setTextChangedListeners();
         fixLocation();
-        setButtonsClickListeners();
+        setDateAndTimeClickListeners();
+        setAddClickListener();
+
+        // Set up database
+        mHelpMePostDatabaseReference = FirebaseDatabase.getInstance().getReference().child("helpMe");
     }
 
     @Override
@@ -71,6 +88,53 @@ public class HelpMePostAddActivity extends AppCompatActivity {
     }
 
     /**
+     * Set text changed listeners for category and title fields
+     */
+    private void setTextChangedListeners() {
+        TextInputLayout categoryField = findViewById(R.id.help_me_post_add_category);
+        categoryField.getEditText().addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                // Do nothing
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                // Do nothing
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                isValidCategory = true;
+            }
+        });
+
+        final TextInputLayout titleField = findViewById(R.id.help_me_post_add_title);
+        titleField.getEditText().addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                // Do nothing
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                // Do nothing
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                if (Validation.isNonEmpty(s)) {
+                    isValidTitle = true;
+                    titleField.setError(null);
+                } else {
+                    isValidTitle = false;
+                    titleField.setError(getString(R.string.error_title));
+                }
+            }
+        });
+    }
+
+    /**
      * Fix user's location in the text field
      */
     private void fixLocation() {
@@ -80,7 +144,7 @@ public class HelpMePostAddActivity extends AppCompatActivity {
     /**
      * Set click listeners for date and time buttons
      */
-    private void setButtonsClickListeners() {
+    private void setDateAndTimeClickListeners() {
         MaterialButton dateButton = findViewById(R.id.help_me_post_add_date_button);
         dateButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -109,6 +173,8 @@ public class HelpMePostAddActivity extends AppCompatActivity {
      * @param dayOfMonth the day of month chosen
      */
     public void processDatePickerResult(int year, int month, int dayOfMonth) {
+        isValidDate = true;
+
         // Create date message
         mDate = new int[]{year, month, dayOfMonth};
         String year_string = Integer.toString(year);
@@ -136,6 +202,8 @@ public class HelpMePostAddActivity extends AppCompatActivity {
      * @param minute the minute chosen
      */
     public void processTimePickerResult(int hourOfDay, int minute) {
+        isNonEmptyTime = true;
+
         // Create time message
         mTime = new int[]{hourOfDay, minute};
         int hour = hourOfDay <= 12 ? hourOfDay : hourOfDay - 12;
@@ -171,11 +239,13 @@ public class HelpMePostAddActivity extends AppCompatActivity {
             if (matchYear && matchMonth && matchDay) {
                 if (c.get(Calendar.HOUR_OF_DAY) > mTime[0] ||
                         (c.get(Calendar.HOUR_OF_DAY) == mTime[0] && c.get(Calendar.MINUTE) > mTime[1])) {
+                    isValidTime = false;
                     timeInputLayout.setError(getString(R.string.error_time));
                     return;
                 }
             }
         }
+        isValidTime = true;
         timeInputLayout.setError(null);
     }
 
@@ -194,5 +264,46 @@ public class HelpMePostAddActivity extends AppCompatActivity {
         }
 
         imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+    }
+
+    /**
+     * Set click listener for add button
+     */
+    private void setAddClickListener() {
+        MaterialButton addButton = findViewById(R.id.help_me_post_add_button);
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!isValidCategory) {
+                    showAlertDialog(getString(R.string.error_category));
+                } else if (!isValidTitle) {
+                    showAlertDialog(getString(R.string.error_title));
+                } else if (!isValidDate) {
+                    showAlertDialog(getString(R.string.error_date));
+                } else if (!isNonEmptyTime) {
+                    showAlertDialog(getString(R.string.error_empty_time));
+                } else if (!isValidTime) {
+                    showAlertDialog(getString(R.string.error_time));
+                } else {
+                    
+                }
+            }
+        });
+    }
+
+    /**
+     * Show alert dialog
+     * @param message the message to be displayed in the dialog
+     */
+    private void showAlertDialog(String message) {
+        new AlertDialog.Builder(this)
+                .setMessage(message)
+                .setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        // User clicked OK button
+                    }
+                })
+                .show();
     }
 }
