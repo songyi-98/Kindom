a3777d9a61c7f1020039b5045d3daa40ee76a21a
SONGYI <e0392524@u.nus.edu>
2020-06-19 07:48:31 +0800

    Check if internet connection is available

--- app/src/main/java/com/example/kindom/Alert.java
+++ app/src/main/java/com/example/kindom/Alert.java
@@ -1,5 +1,6 @@
 package com.example.kindom;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.DialogInterface;
 
@@ -23,6 +24,25 @@ public class Alert {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
                         // User clicked OK button
+                    }
+                })
+                .show();
+    }
+
+    /**
+     * Show alert dialog and finish activity
+     *
+     * @param activity the activity to be finished
+     * @param context the context
+     * @param message the message to be displayed
+     */
+    public static void showAlertDialogAndFinish(final Activity activity, Context context, String message) {
+        new AlertDialog.Builder(context)
+                .setMessage(message)
+                .setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        activity.finish();
                     }
                 })
                 .show();
