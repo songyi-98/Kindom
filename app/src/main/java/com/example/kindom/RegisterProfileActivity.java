f9edd07138a4720ad094e3f3acdd0fdf0b3e658c
Song Yi <song.yi.ang.98@gmail.com>
2020-06-23 23:45:47 +0800

    Use OneMap API to query block number from postal code

--- app/src/main/java/com/example/kindom/RegisterProfileActivity.java
+++ app/src/main/java/com/example/kindom/RegisterProfileActivity.java
@@ -7,10 +7,14 @@ import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.View;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.loader.app.LoaderManager;
+import androidx.loader.content.Loader;
 
 import com.bumptech.glide.Glide;
+import com.example.kindom.utils.AddressLoader;
 import com.example.kindom.utils.Alert;
 import com.example.kindom.utils.Validation;
 import com.google.android.material.button.MaterialButton;
@@ -21,18 +25,20 @@ import java.util.Objects;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
-public class RegisterProfileActivity extends AppCompatActivity {
+public class RegisterProfileActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Integer> {
 
     public static final int PICK_IMAGE = 1;
     public static final String USER_PROFILE_IMAGE_TAG = "USER_PROFILE_IMAGE";
     public static final String USER_NAME_TAG = "USER_NAME";
     public static final String USER_POSTAL_CODE_TAG = "USER_POSTAL_CODE";
+    public static final String USER_BLK_NO_TAG = "USER_BLK_NO";
     public static final String USER_GROUP_TAG = "USER_GROUP";
 
     private CircleImageView mProfileImage;
     private Uri mProfileImageUri;
     private TextInputLayout mNameField;
     private TextInputLayout mPostalCodeField;
+    private Integer mBlkNo = -1;
     private MaterialButtonToggleGroup mUserGroup;
     private MaterialButton mNextButton;
     private boolean isValidProfileImage = false;
@@ -99,6 +105,23 @@ public class RegisterProfileActivity extends AppCompatActivity {
         }
     }
 
+    @NonNull
+    @Override
+    public Loader<Integer> onCreateLoader(int id, @Nullable Bundle args) {
+        return new AddressLoader(this, Integer.parseInt(Objects.requireNonNull(mPostalCodeField.getEditText()).getText().toString()));
+    }
+
+    @Override
+    public void onLoadFinished(@NonNull Loader<Integer> loader, Integer data) {
+        mBlkNo = data;
+    }
+
+    @Override
+    public void onLoaderReset(@NonNull Loader<Integer> loader) {
+        mBlkNo = -1;
+        isValidPostalCode = false;
+    }
+
     /**
      * Validate he inputs of name, postal code and user group
      */
@@ -139,7 +162,6 @@ public class RegisterProfileActivity extends AppCompatActivity {
 
             @Override
             public void afterTextChanged(Editable s) {
-                // TODO: Check postal code validity
                 int len = s.toString().length();
                 if (!Validation.isNonEmpty(s)) {
                     isValidPostalCode = false;
@@ -148,7 +170,9 @@ public class RegisterProfileActivity extends AppCompatActivity {
                     isValidPostalCode = false;
                     mPostalCodeField.setError(getString(R.string.error_postal_code_length));
                 } else {
-                    isValidPostalCode = true;
+                    // Start loader to query postal code received from the intent
+                    getSupportLoaderManager().initLoader(0, null, RegisterProfileActivity.this);
+
                     mPostalCodeField.setError(null);
                 }
             }
@@ -169,6 +193,14 @@ public class RegisterProfileActivity extends AppCompatActivity {
         mNextButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                // Check postal code validity
+                if (mBlkNo == -1) {
+                    Alert.showAlertDialog(RegisterProfileActivity.this, getString(R.string.error_postal_code_processing));
+                }
+                if (mBlkNo != null) {
+                    isValidPostalCode = true;
+                }
+
                 if (!isValidProfileImage) {
                     Alert.showAlertDialog(RegisterProfileActivity.this, getString(R.string.error_profile_image));
                 } else if (!isValidName) {
@@ -182,6 +214,7 @@ public class RegisterProfileActivity extends AppCompatActivity {
                     intent.putExtra(USER_PROFILE_IMAGE_TAG, mProfileImageUri.toString());
                     intent.putExtra(USER_NAME_TAG, Objects.requireNonNull(mNameField.getEditText()).getText().toString());
                     intent.putExtra(USER_POSTAL_CODE_TAG, Integer.parseInt(Objects.requireNonNull(mPostalCodeField.getEditText()).getText().toString()));
+                    intent.putExtra(USER_BLK_NO_TAG, mBlkNo);
                     int checkedId = mUserGroup.getCheckedButtonId();
                     if (checkedId == R.id.user_group_admin) {
                         intent.putExtra(USER_GROUP_TAG, User.USER_GROUP_ADMIN);
