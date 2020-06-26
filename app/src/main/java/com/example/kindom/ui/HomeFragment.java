39bba0c6d3b1bda1f33307aaff7af99657815d3a
SONGYI <e0392524@u.nus.edu>
2020-06-26 23:49:30 +0800

    Add dot indicators to image sliders

--- app/src/main/java/com/example/kindom/ui/HomeFragment.java
+++ app/src/main/java/com/example/kindom/ui/HomeFragment.java
@@ -10,6 +10,8 @@ import androidx.viewpager.widget.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import com.example.kindom.R;
 import com.example.kindom.ui.home.HomeImagePagerAdapter;
@@ -35,23 +37,107 @@ public class HomeFragment extends Fragment {
     }
 
     @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+    public void onViewCreated(@NonNull final View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
         // Initialize images
         ArrayList<Integer> neighbourhoodImages = new ArrayList<>();
         neighbourhoodImages.add(R.drawable.neighbourhood_sample_image_1);
         neighbourhoodImages.add(R.drawable.neighbourhood_sample_image_2);
+
         ArrayList<Integer> localImages = new ArrayList<>();
         localImages.add(R.drawable.local_sample_image_1);
         localImages.add(R.drawable.local_sample_image_2);
 
+        // Initialize slider dots panel
+        final LinearLayout neighbourhoodSliderDotsPanel= view.findViewById(R.id.home_neighbourhood_news_slider);
+        final int neighbourhoodDotsCount = neighbourhoodImages.size();
+        final ImageView[] neighbourhoodDots = new ImageView[neighbourhoodDotsCount];
+        for (int i = 0; i < neighbourhoodDotsCount; i++) {
+            neighbourhoodDots[i] = new ImageView(view.getContext());
+            if (i == 0) {
+                neighbourhoodDots[i].setImageDrawable(view.getContext().getDrawable(R.drawable.slider_dot_active));
+            } else {
+                neighbourhoodDots[i].setImageDrawable(view.getContext().getDrawable(R.drawable.slider_dot_non_active));
+            }
+
+            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+            params.setMargins(8, 0, 8, 0);
+
+            neighbourhoodSliderDotsPanel.addView(neighbourhoodDots[i], params);
+        }
+
+        LinearLayout localSliderDotsPanel = view.findViewById(R.id.home_local_news_slider);
+        final int localDotsCount = localImages.size();
+        final ImageView[] localDots = new ImageView[localDotsCount];
+        for (int i = 0; i < localDotsCount; i++) {
+            localDots[i] = new ImageView(view.getContext());
+            if (i == 0) {
+                localDots[i].setImageDrawable(view.getContext().getDrawable(R.drawable.slider_dot_active));
+            } else {
+                localDots[i].setImageDrawable(view.getContext().getDrawable(R.drawable.slider_dot_non_active));
+            }
+
+            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+            params.setMargins(8, 0, 8, 0);
+
+            localSliderDotsPanel.addView(localDots[i], params);
+        }
+
         // Initialize view pagers and adapters
         ViewPager neighbourhoodViewPager = view.findViewById(R.id.home_neighbourhood_news_view_pager);
-        ViewPager localViewPager = view.findViewById(R.id.home_local_news_view_pager);
         HomeImagePagerAdapter neighbourhoodAdapter = new HomeImagePagerAdapter(view.getContext(), neighbourhoodImages);
-        HomeImagePagerAdapter localAdapter = new HomeImagePagerAdapter(view.getContext(), localImages);
         neighbourhoodViewPager.setAdapter(neighbourhoodAdapter);
+
+        ViewPager localViewPager = view.findViewById(R.id.home_local_news_view_pager);
+        HomeImagePagerAdapter localAdapter = new HomeImagePagerAdapter(view.getContext(), localImages);
         localViewPager.setAdapter(localAdapter);
+
+        // Set page change listeners
+        neighbourhoodViewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+                // Do nothing
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                for (int i = 0; i < neighbourhoodDotsCount; i++) {
+                    if (i == position) {
+                        neighbourhoodDots[i].setImageDrawable(view.getContext().getDrawable(R.drawable.slider_dot_active));
+                    } else {
+                        neighbourhoodDots[i].setImageDrawable(view.getContext().getDrawable(R.drawable.slider_dot_non_active));
+                    }
+                }
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+                // Do nothing
+            }
+        });
+
+        localViewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+                // Do nothing
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                for (int i = 0; i < localDotsCount; i++) {
+                    if (i == position) {
+                        localDots[i].setImageDrawable(view.getContext().getDrawable(R.drawable.slider_dot_active));
+                    } else {
+                        localDots[i].setImageDrawable(view.getContext().getDrawable(R.drawable.slider_dot_non_active));
+                    }
+                }
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+                // Do nothing
+            }
+        });
     }
 }
