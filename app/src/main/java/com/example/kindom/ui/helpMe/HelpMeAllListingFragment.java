0d9a327d23d1ad13020803547a351ff949f1a4a1
SONGYI <e0392524@u.nus.edu>
2020-07-22 05:39:21 +0800

    Add progress indicators and display text for empty Help Me listings

--- app/src/main/java/com/example/kindom/ui/helpMe/HelpMeAllListingFragment.java
+++ app/src/main/java/com/example/kindom/ui/helpMe/HelpMeAllListingFragment.java
@@ -11,13 +11,13 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.example.kindom.R;
 import com.example.kindom.User;
 import com.example.kindom.helpMe.HelpMeAllListingAdapter;
 import com.example.kindom.helpMe.HelpMePost;
 import com.example.kindom.utils.FirebaseHandler;
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -72,21 +72,6 @@ public class HelpMeAllListingFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         mHelpMePosts.clear();
-
-        // Set click listener for FAB
-        FloatingActionButton fab = view.findViewById(R.id.help_me_refresh_fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                mSwipeRefreshLayout.post(new Runnable() {
-                    @Override
-                    public void run() {
-                        mSwipeRefreshLayout.setRefreshing(true);
-                        getPosts();
-                    }
-                });
-            }
-        });
     }
 
     @Override
@@ -114,9 +99,9 @@ public class HelpMeAllListingFragment extends Fragment {
                 mAllPostsRef.child(rc).orderByChild("timeCreated").addListenerForSingleValueEvent(new ValueEventListener() {
                     @Override
                     public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                        for (DataSnapshot userSnapshot: dataSnapshot.getChildren()) {
+                        for (DataSnapshot userSnapshot : dataSnapshot.getChildren()) {
                             if (!Objects.equals(userSnapshot.getKey(), FirebaseHandler.getCurrentUserUid())) {
-                                for (DataSnapshot postSnapshot: userSnapshot.getChildren()) {
+                                for (DataSnapshot postSnapshot : userSnapshot.getChildren()) {
                                     HelpMePost post = postSnapshot.getValue(HelpMePost.class);
                                     mHelpMePosts.add(post);
                                 }
@@ -148,6 +133,9 @@ public class HelpMeAllListingFragment extends Fragment {
         RecyclerView mRecyclerView = mView.findViewById(R.id.help_me_all_listing_recycler_view);
 
         if (getContext() != null) {
+            // Hide progress indicator
+            mView.findViewById(R.id.help_me_all_listing_progress_circular).setVisibility(View.GONE);
+
             // Create an adapter and supply the data to be displayed
             HelpMeAllListingAdapter mAdapter = new HelpMeAllListingAdapter(getContext(), mHelpMePosts);
 
@@ -158,6 +146,12 @@ public class HelpMeAllListingFragment extends Fragment {
             mRecyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
 
             mSwipeRefreshLayout.setRefreshing(false);
+
+            if (mAdapter.getItemCount() == 0) {
+                mView.findViewById(R.id.help_me_all_listing_empty).setVisibility(View.VISIBLE);
+            }
+
+            Toast.makeText(getContext(), R.string.help_me_all_listing_refresh_toast, Toast.LENGTH_LONG).show();
         }
     }
 }
