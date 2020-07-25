bf7f969b35a0716a132f7e8cc6424abedee71cd6
Song Yi <song.yi.ang.98@gmail.com>
2020-07-26 00:39:26 +0800

    Add refresh functionality for HelpMeUserListingFragment

--- app/src/main/java/com/example/kindom/ui/helpMe/HelpMeUserListingFragment.java
+++ app/src/main/java/com/example/kindom/ui/helpMe/HelpMeUserListingFragment.java
@@ -6,12 +6,14 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.example.kindom.R;
 import com.example.kindom.User;
@@ -31,6 +33,7 @@ import java.util.ArrayList;
 public class HelpMeUserListingFragment extends Fragment {
 
     private View mView;
+    private SwipeRefreshLayout mSwipeRefreshLayout;
     private final ArrayList<HelpMePost> mHelpMePosts = new ArrayList<>();
     private DatabaseReference mUserRef;
     private DatabaseReference mUserPostsRef;
@@ -53,6 +56,17 @@ public class HelpMeUserListingFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         mView = inflater.inflate(R.layout.activity_help_me_user_listing_fragment, container, false);
+
+        // Initialize SwipeRefreshLayout
+        mSwipeRefreshLayout = mView.findViewById(R.id.help_me_user_listing_swipe_container);
+        mSwipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                mSwipeRefreshLayout.setRefreshing(true);
+                getPosts();
+            }
+        });
+
         return mView;
     }
 
@@ -76,8 +90,16 @@ public class HelpMeUserListingFragment extends Fragment {
     public void onStart() {
         super.onStart();
 
-        // Add user's posts to list
+        getPosts();
+    }
+
+    /**
+     * Get HelpMePosts from Firebase Database
+     */
+    private void getPosts() {
         mHelpMePosts.clear();
+
+        // Add user's posts to list
         mUserRef.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
@@ -137,11 +159,15 @@ public class HelpMeUserListingFragment extends Fragment {
             // Give the RecyclerView a default layout manager
             mRecyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
 
+            mSwipeRefreshLayout.setRefreshing(false);
+
             if (mAdapter.getItemCount() == 0) {
                 mView.findViewById(R.id.help_me_user_listing_empty).setVisibility(View.VISIBLE);
             } else {
                 mView.findViewById(R.id.help_me_user_listing_empty).setVisibility(View.GONE);
             }
+
+            Toast.makeText(getContext(), R.string.help_me_refresh_toast, Toast.LENGTH_SHORT).show();
         }
     }
 }
